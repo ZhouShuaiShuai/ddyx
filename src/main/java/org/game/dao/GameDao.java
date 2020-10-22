0b54f6f9f76453505364953cabc8ea9dd4bf59b1
ZhuaiShuaishuai <lm99999993@163.com>
2020-10-22 20:59:53 +0800

    a

--- src/main/java/org/game/dao/GameDao.java
+++ src/main/java/org/game/dao/GameDao.java
@@ -61,7 +61,8 @@ public interface GameDao extends JpaRepository<Game, Integer> {
     @Query(value = "select sum(game_money) from yebill where user_id = ?1 group by game_id order by game_id desc limit 1",nativeQuery = true )
     Integer getLastGameMoney(Integer userId);
 
-    @Query(value = "select " +
+    @Query(value =
+            /*"select " +
             "g.id gameid," +
             "g.number num ," +
             "g.number%3 as num3 ," +
@@ -75,14 +76,37 @@ public interface GameDao extends JpaRepository<Game, Integer> {
             "if(g.number % 2 = 0 ,'双','单') ds," +
             "g.id - (select g2.id from game g2 where if(g2.number % 2 = 0 ,'双','单') = if(g.number % 2 = 0 ,'双','单') and g2.id <= g.id ORDER BY g2.id desc limit 1,1) as dsjg," +
             "(select count(*) from (select * from game where number is not null ORDER BY id desc limit 200) g2 where g2.number = g.number GROUP BY g2.number) sjcs " +
+            "from game g where g.number is not null ORDER BY g.id desc limit 200;",*/
+            "select " +
+            "g.id gameid," +
+            "g.number num ," +
+            "g.number%3 as num3 ," +
+            "g.number%4 as num4 ," +
+            "g.number%5 as num5 ," +
+            "if(g.number > 14,'大','小') dx," +
+            "if(g.number BETWEEN 10 AND 17 ,'中','边') zb," +
+            "if(g.number % 2 = 0 ,'双','单') ds " +
             "from game g where g.number is not null ORDER BY g.id desc limit 200;",
             nativeQuery = true)
     List<Map<String,Integer>> find100GameNum();
 
     @Query(value = "select g.number num ," +
-            "g.id - (select g2.id from game g2 where g2.number = g.number and g2.id <= g.id ORDER BY g2.id desc limit 1,1) as jg " +
-            " from game g where g.id in " +
-            " (select max(id) from game where number is not null GROUP BY number) ORDER BY g.number",
+            "(select id from game  where re_time = 150 ORDER BY id desc limit 1) - g.id -1 as jg , " +
+            "(select id from game  where re_time = 150 ORDER BY id desc limit 1) - (select id from game where number >= 14 ORDER BY id desc limit 1) - 1 dxdjg," +
+            "(select id from game  where re_time = 150 ORDER BY id desc limit 1) - (select id from game where number <= 14 ORDER BY id desc limit 1) - 1 dxxjg," +
+            "(select id from game  where re_time = 150 ORDER BY id desc limit 1) - (select id from game where number % 2 = 1 ORDER BY id desc limit 1 ) - 1 dsdjg," +
+            "(select id from game  where re_time = 150 ORDER BY id desc limit 1) - (select id from game where number % 2 = 0 ORDER BY id desc limit 1) - 1 dssjg," +
+            "(select id from game  where re_time = 150 ORDER BY id desc limit 1) - (select id from game where number BETWEEN 10 AND 17 ORDER BY id desc limit 1) - 1 zbzjg," +
+            "(select id from game  where re_time = 150 ORDER BY id desc limit 1) - (select id from game where number < 10 or number >= 17 ORDER BY id desc limit 1) - 1 zbbjg," +
+            "(select count(*) from (select * from game where number is not null ORDER BY id desc limit 200) g2 where g2.number >= 14 ORDER BY id desc limit 200 ) dxdcs," +
+            "(select count(*) from (select * from game where number is not null ORDER BY id desc limit 200) g2 where g2.number < 14 ORDER BY id desc limit 200 ) dxxcs," +
+            "(select count(*) from (select * from game where number is not null ORDER BY id desc limit 200) g2 where g2.number % 2 = 1 ORDER BY id desc limit 200 ) dsdcs," +
+            "(select count(*) from (select * from game where number is not null ORDER BY id desc limit 200) g2 where g2.number % 2 = 0 ORDER BY id desc limit 200 ) dsscs," +
+            "(select count(*) from (select * from game where number is not null ORDER BY id desc limit 200) g2 where g2.number BETWEEN 10 AND 17 ORDER BY id desc limit 200 ) zbzcs," +
+            "(select count(*) from (select * from game where number is not null ORDER BY id desc limit 200) g2 where g2.number < 10 or g2.number > 17 ORDER BY id desc limit 200 ) zbbcs," +
+            "(select count(*) from (select * from game where number is not null ORDER BY id desc limit 200) g2 where g2.number = g.number GROUP BY g2.number) sjcs " +
+            "from game g where g.id in " +
+            "(select max(id) from game where number is not null GROUP BY number) ORDER BY g.number;",
             nativeQuery = true)
     List<Map<String,Integer>> findNumJg();
 
--- src/main/java/org/game/util/UserUtil.java
+++ src/main/java/org/game/util/UserUtil.java
@@ -44,13 +44,22 @@ public class UserUtil {
             userInfo.setGameId(gameId);
             userInfo.setNum(num);
             userInfo.setHeadImg(MD5.random.nextInt(1032)+".jpg");
-            Integer tze = MD5.random.nextInt((i+10)*10*350)+987654;
+            Integer tze = MD5.random.nextInt((i+100)*10*350*3)+987654;
             if(tze>100000000){
                 tze = 100000000;
             }
             userInfo.setTz(tze);
-//            userInfo.setHl((tze/ Magnification.getPlByNum(num) + MD5.random.nextInt(i+100))*Magnification.getPlByNum(num));
-            userInfo.setHl((tze * Magnification.getPlByNum(num) / (MD5.random.nextInt(10)+5)) + Magnification.getPlByNum(num) * (MD5.random.nextInt(i+1)+1000));
+
+            if(tze > 80000000){
+                userInfo.setHl(tze * (MD5.random.nextInt(2)+1)/2 * (MD5.random.nextInt(2)+1) + tze);
+            }else {
+                userInfo.setHl(tze * (MD5.random.nextInt(5)+1)/3 * (MD5.random.nextInt(5)+1) + tze);
+            }
+
+////            userInfo.setHl((tze/ Magnification.getPlByNum(num) + MD5.random.nextInt(i+100))*Magnification.getPlByNum(num));
+////            userInfo.setHl((tze * Magnification.getPlByNum(num) / (MD5.random.nextInt(10)+5)) + Magnification.getPlByNum(num) * (MD5.random.nextInt(i+1)+1000));
+//            Integer bb = (MD5.random.nextInt(5)+1);
+//            userInfo.setHl((tze * (bb/2)) + Magnification.getPlByNum(num) * (MD5.random.nextInt(i+1)+1000)+tze);
             userInfo.setYl(userInfo.getHl()-userInfo.getTz());
             list.add(userInfo);
         }
